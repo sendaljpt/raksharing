74bb1c6009db92cb041d735baef178dbb3d84ef9
fajrin <sendaljpt24@gmail.com>
2019-09-07 11:21:05 +0700

    introduction one

---
+++ introduction-1/2_type_data.py
@@ -0,0 +1,34 @@
+# integer
+a = 10
+print(a)
+print(type(a))
+
+# string
+ab = "saya adalah string"
+print(ab)
+print(type(ab))
+
+# float
+ac = 4.5
+print(ac)
+print(type(ac))
+
+# boolean
+ad = True
+ae = False
+
+print(ad)
+print(ae)
+print(type(ad))
+
+# array/list
+af = [1, 4, 7, 8]
+print(af)
+print(type(af))
+
+# object/dict
+ag = {"nama": "anton", "umur": 25}
+print(ag)
+print(ag['nama'])
+print(ag.get('umur'))
+print(type(ag))
---
+++ introduction-1/3_math.py
@@ -0,0 +1,15 @@
+# penumlahan
+a = 3 + 4
+print(a)
+
+# pengurangan
+b = 10 - 4
+print(b)
+
+# perkalian
+c = 3 * 3
+print(c)
+
+# pembagian
+d = 10 / 3
+print(d)
